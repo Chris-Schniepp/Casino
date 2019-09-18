24f65c72f10395ea71b0e28394c140b259544a41
Chris-Schniepp <chrisschniepp@dpengineering.org>
2019-09-18 09:27:03 -0700

    added new file 50-50

---
+++ 50-50.py
@@ -0,0 +1,6 @@
+print("Hello valued guest, Welcome to the worlds fairest Casino"
+      "Where anyone can become rich over night!")
+
+
+print("Our first game is called the 50-50. As the name suggests the"
+      "game is simple, the computer will randomly generate a fact")
